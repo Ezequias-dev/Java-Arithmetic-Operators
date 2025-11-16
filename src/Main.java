14cfd341128b91a521503dca935dff56c605fd7b
ezequias-dev <ezequiasd30@gmail.com>
2025-11-16 15:37:54 -0300

    Some improvements

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        // 1. Some dois inteiros a = 8 e b = 13 e imprima o resultado.
-        int a = 8;
-        int b = 13;
+        // 1. Add two integers a = 8 and b = 13 and print the result.
+        int a = 8, b = 13;
 
         System.out.println(a + b);
 
-        // 2. Calcule a área de um retângulo com double largura = 5.5 e double altura = 3.2. Imprima com mensagem.
-        double largura = 5.5;
-        double altura = 3.2;
-        double area = largura * altura;
+        // 2. Calculate the area of a rectangle with double width = 5.5 and double height = 3.2. Print with message.
+        double width = 5.5, height = 3.2;
+        double area = width * height;
 
-        System.out.println(area);
+        System.out.println("Area: " + area);
 
-        // 3. Qual o resultado de System.out.println(5 + 3 * 2);? Explique por que
-        System.out.println(5 + 3 * 2);
+        // 3. What is the result of System.out.println(5 + 3 * 2);? Explain why
         /*
             11 because the operator "*" has precedence greater than +,
             therefore, first it is multiplied then added
         */
+        System.out.println(7 / 2);
+        System.out.println(7 / 2.0);
+        System.out.println((double) 7 / 2);
     }
 }
