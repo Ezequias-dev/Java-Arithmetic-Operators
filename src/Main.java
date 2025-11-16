b4861e8747f932a0f2d0c1a2037b952c2e42564e
ezequias-dev <ezequiasd30@gmail.com>
2025-11-16 14:53:57 -0300

    1. Some dois inteiros a = 8 e b = 13 e imprima o resultado.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 1. Some dois inteiros a = 8 e b = 13 e imprima o resultado.
+        int a = 8;
+        int b = 13;
+
+        System.out.println(a + b);
+    }
+}
